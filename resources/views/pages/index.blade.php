de5e62138b9ac0c79b126c90490716519e29e6d5
antoniolaikauf <antonio.laikauf00@gmail.com>
2024-02-12 15:31:26 +0100

    add: stampato tutti i dati a schermo

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,7 +7,10 @@
 <ul>
     @foreach ($movies as $movie)
     <li>
-
+        {{$movie['title']}}
+        {{$movie['nationality']}}
+        {{$movie['date']}}
+        {{$movie['vote']}}
     </li>
     @endforeach
 </ul>
